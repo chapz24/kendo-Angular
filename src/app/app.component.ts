744a831f5cce7bcdb8011151e72c86f4826eb728
Chapa Wellappili <c.wellappili@dataquint.nl>
2017-12-06 17:40:52 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import { products } from './products';
+import { Observable } from 'rxjs/Rx';
+import { CategoriesService } from './northwind.service';
+
+import { State } from '@progress/kendo-data-query';
+
+import {
+  GridDataResult,
+  DataStateChangeEvent
+} from '@progress/kendo-angular-grid';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +17,19 @@ import { products } from './products';
 })
 
 export class AppComponent {
-  debugger;
-  title = 'Grid Data';
-  private gridData: any[] = products;
+  public view: Observable<GridDataResult>;
+  public state: State = {
+      skip: 0,
+      take: 5
+  };
+
+  constructor(private service: CategoriesService) {
+    this.view = service;
+    this.service.query(this.state);
+}
+
+public dataStateChange(state: DataStateChangeEvent): void {
+    this.state = state;
+    this.service.query(state);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 // Import the Animations module
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,6 +10,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Kendo UI for Angular 2
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { GridModule } from '@progress/kendo-angular-grid';
+
+import { AppComponent } from './app.component';
+import { CategoriesService }   from './northwind.service';
 
 @NgModule({
   declarations: [
@@ -19,6 +22,7 @@ import { GridModule } from '@progress/kendo-angular-grid';
     BrowserModule,
     FormsModule,
     HttpModule,
+    HttpClientModule,
 
     //Register the modules
     BrowserAnimationsModule,
@@ -26,7 +30,7 @@ import { GridModule } from '@progress/kendo-angular-grid';
     GridModule
 
   ],
-  providers: [],
+  providers: [CategoriesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
